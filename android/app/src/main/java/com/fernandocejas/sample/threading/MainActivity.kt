894e3e90ed5c1d78eff0c2b628c82fa83e25c1d5
Fernando Cejas <fcejas@gmail.com>
2017-12-02 16:13:18 +0100

    Add Threads activity

--- android/app/src/main/java/com/fernandocejas/sample/threading/MainActivity.kt
+++ android/app/src/main/java/com/fernandocejas/sample/threading/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.fernandocejas.sample.threading
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import com.fernandocejas.sample.threading.threads.ThreadsActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnThreads.setOnClickListener { navigateTo(ThreadsActivity::class.java) }
+    }
+
+    private fun navigateTo(activity: Class<ThreadsActivity>) {
+        startActivity(Intent(this, activity))
     }
 }
---
+++ android/app/src/main/java/com/fernandocejas/sample/threading/threads/ThreadsActivity.kt
@@ -0,0 +1,13 @@
+package com.fernandocejas.sample.threading.threads
+
+import android.app.Activity
+import android.os.Bundle
+import com.fernandocejas.sample.threading.R
+
+class ThreadsActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_threads)
+    }
+}
