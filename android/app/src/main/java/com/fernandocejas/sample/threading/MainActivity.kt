3e79c4b745541d824ad8552542072f5cabc77ac5
Fernando Cejas <fcejas@gmail.com>
2017-12-02 15:03:54 +0100

    Base project

---
+++ android/app/src/main/java/com/fernandocejas/sample/threading/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fernandocejas.sample.threading
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
