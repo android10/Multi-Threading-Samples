d26f1614d9952fcec058bb0d1fe0af4d3f604af1
Fernando Cejas <fcejas@gmail.com>
2017-12-08 02:48:50 +0100

    Add Sequential Example RxJava 2

--- android/app/src/main/java/com/fernandocejas/sample/threading/rxjava/WordCount.kt
+++ android/app/src/main/java/com/fernandocejas/sample/threading/rxjava/WordCount.kt
@@ -1,11 +1,43 @@
 package com.fernandocejas.sample.threading.rxjava
 
-import com.fernandocejas.sample.threading.threads.WordCount
+import android.util.Log
+import com.fernandocejas.sample.threading.data.Pages
+import com.fernandocejas.sample.threading.data.Source
+import com.fernandocejas.sample.threading.data.Words
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
 
 class WordCount {
     private val LOG_TAG = WordCount::class.java.canonicalName
 
-    fun runSequentially() {
+    private val counts: HashMap<String, Int?> = HashMap()
 
+    fun runSequentially() {
+        val startTime = System.currentTimeMillis()
+
+        val observable = Observable.fromCallable {
+            val pagesOne = Pages(0, 5000, Source().wikiPagesBatchOne())
+            pagesOne.forEach { page -> Words(page.text).forEach { countWord(it) } }
+
+            val pagesTwo = Pages(0, 5000, Source().wikiPagesBatchTwo())
+            pagesTwo.forEach { page -> Words(page.text).forEach { countWord(it) } }
+        }
+
+        observable
+                .doOnComplete { logData(System.currentTimeMillis() - startTime) }
+                .subscribeOn(Schedulers.single())
+                .subscribe()
+    }
+
+    private fun countWord(word: String) {
+        when(counts.containsKey(word)) {
+            true -> counts[word] = counts[word]?.plus(1)
+            false -> counts[word] = 1
+        }
+    }
+
+    private fun logData(time: Long) {
+        Log.d(LOG_TAG, "Number of elements: ${counts.size}")
+        Log.d(LOG_TAG, "Execution Time: $time ms")
     }
 }
