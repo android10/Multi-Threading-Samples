bc67e0e3d5f6dc93e2629e4a98fe1fc13ab4a6b7
Fernando Cejas <fcejas@gmail.com>
2017-12-08 02:29:28 +0100

    Add RxJava 2 dependency plus some cleanup

---
+++ android/app/src/main/java/com/fernandocejas/sample/threading/rxjava/WordCount.kt
@@ -0,0 +1,11 @@
+package com.fernandocejas.sample.threading.rxjava
+
+import com.fernandocejas.sample.threading.threads.WordCount
+
+class WordCount {
+    private val LOG_TAG = WordCount::class.java.canonicalName
+
+    fun runSequentially() {
+
+    }
+}
