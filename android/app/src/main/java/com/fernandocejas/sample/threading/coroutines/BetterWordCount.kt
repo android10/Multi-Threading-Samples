83a05aa87bf5140e0bea26d939c34510d3184b9a
Fernando Cejas <fcejas@gmail.com>
2018-01-25 12:12:15 +0100

    Add merge results for BetterWordCount with coroutines.

--- android/app/src/main/java/com/fernandocejas/sample/threading/coroutines/BetterWordCount.kt
+++ android/app/src/main/java/com/fernandocejas/sample/threading/coroutines/BetterWordCount.kt
@@ -1,5 +1,7 @@
 package com.fernandocejas.sample.threading.coroutines
 
+import android.annotation.TargetApi
+import android.os.Build.VERSION_CODES
 import android.util.Log
 import com.fernandocejas.sample.threading.data.Pages
 import com.fernandocejas.sample.threading.data.Source
@@ -23,7 +25,7 @@ class BetterWordCount(source: Source) {
                 val two = async(CommonPool) { counter(350.rangeTo(700), filePagesOne) }
                 val three = async(CommonPool) { counter(0.rangeTo(349), filePagesTwo) }
                 val four = async(CommonPool) { counter(350.rangeTo(700), filePagesTwo) }
-                one.await(); two.await(); three.await(); four.await()
+                mergeResults(one.await(), two.await(), three.await(), four.await())
             }
             logData(time)
         }
@@ -46,4 +48,14 @@ class BetterWordCount(source: Source) {
     private fun logData(time: Long) {
         Log.d(LOG_TAG, "Execution Time: $time ms")
     }
+
+    private fun mergeResults(vararg results: HashMap<String, Int?>) {
+        //TODO: implement this and refactor num
+        //TODO: use the function mergeReduce below to merge results.
+    }
+
+    //TODO: improve this function
+    @TargetApi(VERSION_CODES.N)
+    fun <K, V> Map<K, V>.mergeReduce(other: Map<K, V>, reduce: (V, V) -> V = { _, b -> b }): Map<K, V> =
+            this.toMutableMap().apply { other.forEach { merge(it.key, it.value, reduce) } }
 }
